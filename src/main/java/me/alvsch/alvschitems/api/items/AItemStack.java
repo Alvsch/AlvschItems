dfa4d8b6f3b5d9418308663d4825af5f39e1eb5c
BuildTools <unconfigured@null.spigotmc.org>
2023-02-27 18:18:10 +0100

    AItemStack

--- src/main/java/me/alvsch/alvschitems/api/items/AItemStack.java
+++ src/main/java/me/alvsch/alvschitems/api/items/AItemStack.java
@@ -1,10 +1,18 @@
 package me.alvsch.alvschitems.api.items;
 
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class AItemStack extends ItemStack {
 
 	private String id;
+
+	public AItemStack(Material material) {
+		super(material);
+	}
+	public AItemStack(Material material, int amount) {
+		super(material, amount);
+	}
 
 	public String getId() {
 		return id;
